736ccfef7f743087dc5a96823c5fd46dedf44e85
sano_yuya <k021g1256@g.neec.ac.jp>
2022-07-02 16:44:15 +0900

    10

---
+++ Assets/Player/Bomb.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    //発生させるパーティクルを生成
+    public GameObject particle;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //キーボードのBキーが押されたら
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            //タグが同じオブジェクトをすべて取得する
+            GameObject[] enemyBulletObjects = GameObject.FindGameObjectsWithTag("EnemyBullet");
+
+            //上で取得したすべてのオブジェクトを消滅させる
+            for(int i = 0; i < enemyBulletObjects.Length; i++)
+            {
+                Destroy(enemyBulletObjects[i].gameObject);
+            }
+
+            //パーティクルを持ったオブジェクトを生成する
+            Instantiate(particle, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
